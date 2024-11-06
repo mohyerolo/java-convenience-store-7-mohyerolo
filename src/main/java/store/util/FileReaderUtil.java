7eed9529bfa80ccdebd1f3447ed27ac715fd38a5
mohyerolo <garam2101@naver.com>
2024-11-06 21:28:29 +0900

    feat: 주어진 파일 읽는 Util

---
+++ src/main/java/store/util/FileReaderUtil.java
@@ -0,0 +1,47 @@
+package store.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReaderUtil {
+
+    private static final String FILE_NOT_FOUND = "[ERROR] 파일을 찾을 수 없습니다";
+    private static final String FILE_READ_ERROR = "[ERROR] 파일을 읽는 중 에러가 발생했습니다.";
+
+    public static List<String> readFile(String fileName) {
+        InputStream inputStream = getInputStreamByFileName(fileName);
+        return readLines(inputStream);
+    }
+
+    private static InputStream getInputStreamByFileName(String fileName) {
+        InputStream resourceAsStream = FileReaderUtil.class.getClassLoader().getResourceAsStream(fileName);
+        if (resourceAsStream == null) {
+            throw new IllegalArgumentException(FILE_NOT_FOUND);
+        }
+        return resourceAsStream;
+    }
+
+    private static List<String> readLines(InputStream inputStream) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            return readAllLines(br);
+        } catch (IOException e) {
+            System.out.println(FILE_READ_ERROR);
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    private static List<String> readAllLines(BufferedReader br) throws IOException {
+        List<String> lines = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+        }
+        return lines;
+    }
+}
---
+++ src/test/java/store/util/FileReaderUtilTest.java
@@ -0,0 +1,42 @@
+package store.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class FileReaderUtilTest {
+    private static final String FILE_NOT_FOUND = "[ERROR] 파일을 찾을 수 없습니다!";
+    private static final String PRODUCTS_FILE = "products.md";
+    private static final int PRODUCTS_FILE_ROW = 17;
+
+    private static final String PROMOTION_FILE = "promotions.md";
+
+    private static final int PROMOTION_FILE_ROW = 4;
+
+    @Test
+    void 읽을_파일이_없음() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> FileReaderUtil.readFile("notFound.txt"))
+                .withMessage(FILE_NOT_FOUND);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {PRODUCTS_FILE, PROMOTION_FILE})
+    void 파일_읽기_성공(String fileName) {
+        assertThat(FileReaderUtil.readFile(fileName)).isNotEmpty();
+    }
+
+    @Test
+    void products_파일_읽어서_list반환() {
+        assertThat(FileReaderUtil.readFile(PRODUCTS_FILE).size()).isEqualTo(PRODUCTS_FILE_ROW);
+    }
+
+    @Test
+    void prmotions_파일_읽어서_list반환() {
+        assertThat(FileReaderUtil.readFile(PROMOTION_FILE).size()).isEqualTo(PROMOTION_FILE_ROW);
+    }
+}
