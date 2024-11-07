8f8c7ec3dfbdfac3e46dba1132bf6861953eeb2d
mohyerolo <garam2101@naver.com>
2024-11-08 00:37:41 +0900

    refactor: 입력 검증 파일 문구 수정

--- src/main/java/store/validator/DataTypeValidator.java
+++ src/main/java/store/validator/DataTypeValidator.java
@@ -7,22 +7,20 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class DataTypeValidator {
-    private static final String FILE_STRING_EXP = "^(?!\\s*$)(?!\\s).+";
-    private static final String FILE_INT_EXP = "^[\\d]+$";
-    private static final String NOT_STRING_FIELD = "문자가 없는 칸이 존재합니다.";
-    private static final String NOT_INT_FIELD = "숫자가 필요한 부분에 숫자가 아닌 문자가 들어가있습니다.";
-    private static final String NOT_DATE_FIELD = "yyyy-MM-dd 날짜 형식이 필요한 부분에 다른 데이터가 들어가있습니다.";
+    private static final String STRING_EXP = "^(?!\\s*$)(?!\\s).+";
+    private static final String INT_EXP = "^[\\d]+$";
+    private static final String INPUT_TYPE_ERROR = "올바르지 않은 형식으로 입력했습니다.";
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static void validateString(final String input) {
-        if (!input.matches(FILE_STRING_EXP)) {
-            throw new CustomIllegalArgException(NOT_STRING_FIELD);
+        if (!input.matches(STRING_EXP)) {
+            throw new CustomIllegalArgException(INPUT_TYPE_ERROR);
         }
     }
 
     public static void validateInt(final String input) {
-        if (!input.matches(FILE_INT_EXP)) {
-            throw new CustomIllegalArgException(NOT_INT_FIELD);
+        if (!input.matches(INT_EXP)) {
+            throw new CustomIllegalArgException(INPUT_TYPE_ERROR);
         }
     }
 
@@ -30,7 +28,7 @@ public class DataTypeValidator {
         try {
             LocalDate.parse(input, dateTimeFormatter);
         } catch (DateTimeParseException e) {
-            throw new CustomIllegalArgException(NOT_DATE_FIELD);
+            throw new CustomIllegalArgException(INPUT_TYPE_ERROR);
         }
 
     }
