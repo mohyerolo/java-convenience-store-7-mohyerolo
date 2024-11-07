893da8544b1983a89ee197aaf57dfd63050146f3
mohyerolo <garam2101@naver.com>
2024-11-08 00:36:58 +0900

    fix: 재입력 문장 수정

--- src/main/java/store/exception/CustomIllegalArgException.java
+++ src/main/java/store/exception/CustomIllegalArgException.java
@@ -2,8 +2,9 @@ package store.exception;
 
 public class CustomIllegalArgException extends IllegalArgumentException {
     private static final String PREFIX = "[ERROR] ";
+    private static final String SUFFIX = " 다시 입력해 주세요.";
 
     public CustomIllegalArgException(String message) {
-        super(PREFIX + message);
+        super(PREFIX + message + SUFFIX);
     }
 }
