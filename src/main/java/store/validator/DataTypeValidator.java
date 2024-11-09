fd38379bd9d221559e7d5626f3586ac476eef434
mohyerolo <garam2101@naver.com>
2024-11-09 23:48:41 +0900

    fix: 잘못된 입력 에러 문구 수정

--- src/main/java/store/validator/DataTypeValidator.java
+++ src/main/java/store/validator/DataTypeValidator.java
@@ -11,7 +11,7 @@ public class DataTypeValidator {
     private static final String INT_EXP = "^[\\d]+$";
     private static final String Y_OR_N_REG_EXP = "[YN]";
     private static final String INPUT_TYPE_ERROR = "올바르지 않은 형식으로 입력했습니다.";
-    private static final String INPUT_ERROR = "잘못된 입력입니다. 다시 입력해 주세요";
+    private static final String INPUT_ERROR = "잘못된 입력입니다.";
 
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
